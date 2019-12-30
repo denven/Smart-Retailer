c18d3ee765445355128278d2741039418fc0733a
Chengwen <myregs@foxmail.com>
2019-12-30 01:25:12 -0800

    Added routes used to response frontend

---
+++ express-back-end/routes/routes.js
@@ -0,0 +1,18 @@
+// express routes for intercation with react front end
+
+module.exports = function(router, db) {
+    
+  // receive video file from front end and then upload it into s3 bucket
+  // router.post();
+
+  // accept command from front end to start an analysis of faces in video
+
+
+
+  // return video analysis status for frontend
+  // router.get();
+
+  // return faces data for front end
+  // router.get();
+
+};
