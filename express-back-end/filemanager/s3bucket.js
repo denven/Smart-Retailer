e83a0aaa178ae037abfe4a6b4dd468bf7cbb7f10
Chengwen <myregs@foxmail.com>
2019-12-30 01:24:25 -0800

    Added src directory for backend managing video,picture,s3bucket files

---
+++ express-back-end/filemanager/s3bucket.js
@@ -0,0 +1,9 @@
+// functions to process files in s3 buckets
+
+// upload video file to s3
+
+
+
+// upload cropped faces' images to s3
+
+
---
+++ express-back-end/filemanager/videos.js
@@ -0,0 +1,7 @@
+// fuctions used to operate video files
+
+
+// take screenshots for frames containing faces
+
+
+// crop faces from each frame screenshot
