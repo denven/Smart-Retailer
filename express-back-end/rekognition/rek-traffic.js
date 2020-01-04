a0269ce02ad984bf1e216ee834528fee23a3dfe8
Chengwen <myregs@foxmail.com>
2020-01-03 22:48:46 -0800

    Addd getVideoTraffic function

--- express-back-end/rekognition/rek-traffic.js
+++ express-back-end/rekognition/rek-traffic.js
@@ -1,12 +1,8 @@
 // This module will use large memory
-const _ = require('lodash');
-
 const {AWS, s3, rekognition, 
   APP_VIDEO_BUCKET_NAME, APP_FACES_BUCKET_NAME, APP_FRAMES_BUCKET_NAME, APP_REK_SQS_NAME,
   APP_REK_COLLECTION_ID, BUCKET_MAX_KEYS,  APP_ROLE_ARN, APP_SNS_TOPIC_ARN,
   awsServiceStart, deleteSQSHisMessages, getSQSMessageSuccess } = require('./aws-servies');
-
-const video = require('../filemanager/videos');
 
 const startPersonTracking = (videoKey) => {
 
@@ -44,9 +40,9 @@ const getPersonsInVideo = (trackData) => {
     allPersonsData.push(newItem);
   }
 
-  let person = {show_timestamp: '', leave_timestamp: '', index: '' };
   let allPersons = [];
   let oldIndex = -1;
+  let person = {show_timestamp: '', leave_timestamp: '', index: '' };
   for (const item of allPersonsData) {
 
     if(item.Index !== oldIndex) { 
@@ -69,8 +65,36 @@ const getPersonsInVideo = (trackData) => {
 
 };
 
+const getVideoTraffic = (persons) => {
+
+  return persons.map((person, index) => {
+    return {
+      timestamp: person.show_timestamp,
+      count: ++index
+    }
+  });
+
+}
+
 const s3_video_key = 'sample-3.mp4';
 const video_local_path = '/home/chengwen/lighthouse/final/Demo/Videos/sample-.mp4';
+
+const getPersonsTracking = (jobId) => {
+
+  let nextToken = '';
+  do { 
+    const params = { JobId: jobId, MaxResults: 1000, NextToken: nextToken, SortBy: "INDEX"};  // by PERSON INDEX
+    rekognition.getPersonTracking(params, (err, trackData) => {
+      if (!err) {
+        let persons = getPersonsInVideo(trackData);
+        console.log(`Found ${persons.length} persons in video ${s3_video_key}`);
+        nextToken = trackData.NextToken || '';
+      } else {
+        console.log(err, err.stack);
+      }
+    });
+  } while (nextToken);
+}
 
 const startPersonTrackingAnalysis = (s3_video_key) => {
   
@@ -79,27 +103,23 @@ const startPersonTrackingAnalysis = (s3_video_key) => {
     // start a new task when SQS is empty
     startPersonTracking(s3_video_key).then((task) => {
 
-      // TODO: when total number of faces > 1000 for the long duration videos
       console.log(`StartPersonTracking..., JobId: ${task.JobId}`);   
-      getSQSMessageSuccess(APP_REK_SQS_NAME, task.JobId).then((status) => {
-
-        const params = { JobId: task.JobId, MaxResults: 1000, NextToken: "", SortBy: "INDEX"};  // by PERSON INDEX
-        rekognition.getPersonTracking(params, (err, trackData) => {
-          if (!err) {
-            console.log(trackData);
-            let persons = getPersonsInVideo(trackData);
-            console.log(`There are ${persons.length} persons in video ${s3_video_key}`);
-          } else {
-            console.log(err, err.stack);
-          }
-        });
-
+      getSQSMessageSuccess(APP_REK_SQS_NAME, task.JobId)
+      .then(() => {
+        let allPerons = getPersonsTracking(task.JobId);
+        getVideoTraffic(allPerons);
       });
-    }).catch((err) => console.log("Failed to track persons from video on S3:", err.stack));
+
+    }).catch((err) => console.log("Failed to track persons in video on S3:", err.stack));
+
   });
 
 };
 
-// call this function when click 
-startPersonTrackingAnalysis(s3_video_key);
+// test code
+// startPersonTrackingAnalysis(s3_video_key);
+// getPersonsTracking('15409357db4f5ec53f4d3d3722f4a41d849ab02b41f6d5280c8464e04c183b3a');
 
+module.exports = {
+  startPersonTrackingAnalysis
+}
