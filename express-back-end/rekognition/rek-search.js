150acc1fe1dc59a566bfbbf0d67ff7f18d7cded5
Chengwen <myregs@foxmail.com>
2020-01-05 01:14:00 -0800

    Changed the nested function getUniqFaceDetails into chains style

--- express-back-end/rekognition/rek-search.js
+++ express-back-end/rekognition/rek-search.js
@@ -11,10 +11,10 @@ const _ = require('lodash');
 const {rekognition,
   APP_VIDEO_BUCKET_NAME, APP_FACES_BUCKET_NAME, APP_REK_SQS_NAME,
   APP_REK_DB_COLLECTION_ID, APP_REK_TEMP_COLLECTION_ID, APP_ROLE_ARN, APP_SNS_TOPIC_ARN,
-   deleteSQSHisMessages, getSQSMessageSuccess } = require('./aws-servies');
+  deleteSQSHisMessages, getSQSMessageSuccess } = require('./aws-servies');
 
 const startFaceSearch = (videoKey, collectionId) => {
-
+  
   return new Promise((resolve, reject) => {
 
     const params = {
@@ -24,7 +24,7 @@ const startFaceSearch = (videoKey, collectionId) => {
       // ClientRequestToken: "startFaceDetectionToken",
       // FaceAttributes: "ALL",  // not supported for this API
       CollectionId: collectionId,
-      FaceMatchThreshold: 95,
+      FaceMatchThreshold: 90,
       JobTag: "startFaceSearch",
       NotificationChannel: {
         RoleArn: APP_ROLE_ARN, 
@@ -71,9 +71,12 @@ const getPersonsInVideo = (data) => {
   let oldIndex = -1;
   let allPersons = [];  
   let person = {};
+
+  console.log(`Get ${data.Persons.length} faces data`)
   for (const item of data.Persons) {   
     
     if(item.Person.Index !== oldIndex) { 
+      console.log(item.Person.Index);
 
       if(oldIndex >= 0) {
         let newObj = JSON.parse(JSON.stringify(person));
@@ -86,13 +89,11 @@ const getPersonsInVideo = (data) => {
         "BoundingBox": item.Person.Face.BoundingBox,
         "Confidence": item.Person.Face.Confidence
       };
-
-      allPersons.push(person);
     }
   }
 
   allPersons.push(person); //last person
-  console.log(`111`, allPersons);
+  console.log(allPersons);
   return allPersons;
 
 };
@@ -100,48 +101,65 @@ const getPersonsInVideo = (data) => {
 // when job is succedded in sqs, call this function
 const getFaceSearch = (jobId) => {
 
-  let nextToken = '';
-  let persons = [];
-  // do { 
-    const params = { JobId: jobId, MaxResults: 1000, NextToken: nextToken, SortBy: "INDEX"};  // by PERSON INDEX
-    rekognition.getFaceSearch(params, (err, data) => {
-      if (!err) {
-        persons = getPersonsInVideo(data);
-        console.log(`Found ${persons.length} persons in video ${s3_video_key}`);
-        nextToken = data.NextToken || '';
-      } else {
-        console.log(err, err.stack);
-      }
-    });
-  // } while (nextToken);
+  return new Promise((resolve, reject) => {
+    
+    let nextToken = '';
+    let persons = [];
 
-  return persons;
+    // do { 
+      const params = { JobId: jobId, MaxResults: 1000, NextToken: nextToken, SortBy: "INDEX" };  // by PERSON INDEX
+      rekognition.getFaceSearch(params, (err, data) => {  
+        if(!err) {
+          persons = getPersonsInVideo(data);
+          console.log(`Recognized ${persons.length} persons in video ${s3_video_key}`);
+          nextToken = data.NextToken || '';
+          resolve(persons);
+        } else {
+          reject(err);
+        }
+      });
+    // } while (nextToken);
+  });
+  // console.log(`getFaceSearch in rek-search`, persons);
+  // return persons;
 
 }
 
 // entry function for call api startFaceSearch
-const getUniqFaceDetails = (videoKey, collectionId) => {
+async function getUniqFaceDetails (videoKey, collectionId) {
    
-  deleteSQSHisMessages(APP_REK_SQS_NAME).then( () => { 
-    
-    // start a new task when SQS is empty
-    startFaceSearch(videoKey, collectionId).then((task) => {
+  deleteSQSHisMessages(APP_REK_SQS_NAME)
+  .then( () => { return startFaceSearch(videoKey, collectionId); })
+  .then((task) => {
+    console.log(`StartFaceSearch..., JobId: ${task.JobId}`);   
+    getSQSMessageSuccess(APP_REK_SQS_NAME, task.JobId).then((status) => {
+      console.log(`Job ${status ? 'SUCCEEDED' : 'NOT_DONE'} from SQS query`);
+      return getFaceSearch(task.JobId); // this is async 
+    }); //.then((data) => console.log(data))
+  })
+  .catch((err) => console.log("Failed to track persons in video on S3:", err.stack));
 
-      console.log(`StartFaceSearch..., JobId: ${task.JobId}`);   
-      getSQSMessageSuccess(APP_REK_SQS_NAME, task.JobId).then((status) => {
-        console.log(`Job status is ${status} in SQS`);
-        let allPerons = getFaceSearch(task.JobId);  // this is async
-      });
+  // deleteSQSHisMessages(APP_REK_SQS_NAME).then( () => { 
+  //   // start a new task when SQS is empty
+  //   startFaceSearch(videoKey, collectionId).then((task) => {
 
-    }).catch((err) => console.log("Failed to track persons in video on S3:", err.stack));
+  //     console.log(`StartFaceSearch..., JobId: ${task.JobId}`);   
+  //     getSQSMessageSuccess(APP_REK_SQS_NAME, task.JobId).then((status) => {
+  //       console.log(`Job ${status ? 'SUCCEEDED' : 'NOT_DONE'} from SQS query`);
+  //       let allPerons = (async () => await getFaceSearch(task.JobId))();  // this is async
+  //       console.log('all perosns in rek-search module:', allPerons);
 
-  });
+  //       return allPerons;
+  //     });
 
+  //   }).catch((err) => console.log("Failed to track persons in video on S3:", err.stack));
+
+  // });
 };
 
 // call this function when click 
 
-getUniqFaceDetails(s3_video_key, APP_REK_TEMP_COLLECTION_ID);
+// getUniqFaceDetails(s3_video_key, APP_REK_TEMP_COLLECTION_ID);
 // getFaceSearch('aba023ce3b8f0a20159273908708be5fc350f65aed2ecf2e2c370ae51a29d1a9');
 
 module.exports = { getUniqFaceDetails };
