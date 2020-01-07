38ba6cbe2797cc42654eac82b23f059b6d14dc22
Chengwen <myregs@foxmail.com>
2020-01-07 01:56:28 -0800

    Added getVideoDimension supporting landscape and portrait mode videos

--- express-back-end/filemanager/videos.js
+++ express-back-end/filemanager/videos.js
@@ -1,36 +1,69 @@
 const helper = require('../helpers/helpers');
 const ffmpeg = require('fluent-ffmpeg');
+// const getDimensions = require('get-video-dimensions');
+
 const sharp = require('sharp');
 const path = require('path');
 const shell = require('shelljs');
 
 const s3Client = require('./s3bucket');
+const __demoRootDir = path.join(require('os').homedir(), 'lighthouse', 'final', 'Demo');
+
 const APP_FACES_BUCKET_NAME = 'retailer-faces';
 
 //TODO: recieve file from front end and save to video path
 
 // Create folders used for processing different type of files 
-const prepareDataDirectories = () => {
+const prepareDataDirectories = (videoFileName) => {
 
-  const userHome = require('os').homedir();
-  __dirname = path.join(userHome, 'lighthouse', 'final', 'Demo');
+  shell.mkdir('-p', path.join(__demoRootDir, 'Videos'));
+  shell.mkdir('-p', path.join(__demoRootDir, 'Frames'));
+  shell.mkdir('-p', path.join(__demoRootDir, 'Faces'));
 
-  shell.mkdir('-p', path.join(__dirname, 'Videos'));
-  shell.mkdir('-p', path.join(__dirname, 'Frames'));
-  shell.mkdir('-p', path.join(__dirname, 'Faces'));
+}
+
+// Get Video Dimension
+async function getVideoDimension (videoFileName) {
+  
+  const videoFullName = path.join(__demoRootDir, 'Videos', videoFileName);
+  // let dimension = await getDimensions(videoFullName);
+  const dimension = {};
+  ffmpeg.ffprobe(videoFullName, (err, metadata) => {
+    if(!err) {
+      console.log(metadata);
+      dimension.width = metadata.streams[0].width;
+      dimension.height= metadata.streams[0].height;
+      dimension.duration = metadata.streams[0].duration;
+      dimension.rotate = metadata.streams[0].tags.rotate;
+      if(metadata.streams[0].tags.rotate) {
+        dimension.width = metadata.streams[0].height;
+        dimension.height= metadata.streams[0].width;
+      }
+    }
+  });
+
+  // can not make ffprobe become a promise, so we use setTimeout
+  return new Promise ( (resolve, reject) => {
+    setTimeout(() => {
+      if(dimension) resolve(dimension);
+      else reject(`Failed to get video dimension`)
+    }, 1000);
+  });
+
 }
 
 // Take screentshots for all the frames which is detected with faces
 // and Save the frame images into a folder named by filename in 'Frames' folder
-const takeScreenshots = (frames, videoFileName) => {
+const takeScreenshots = (frames, videoFileName, vidDimension) => {
 
   let oldTimestamp = -1;
   let framesPromises = [];
 
-  const frameImgPath = path.join(__dirname, 'Frames', videoFileName);
-  const videoFullName = path.join(__dirname, 'Videos', videoFileName);
-  shell.mkdir(frameImgPath);
+  const frameImgPath = path.join(__demoRootDir, 'Frames', videoFileName);
+  const videoFullName = path.join(__demoRootDir, 'Videos', videoFileName);
+  shell.mkdir('-p', frameImgPath);
 
+  const sizeByDimension = `${vidDimension.width}x${vidDimension.height}`;
   for(const frame of frames) {
 
     let newTimestamp = helper.msTohhmmssmmm(frame.Timestamp); 
@@ -44,31 +77,32 @@ const takeScreenshots = (frames, videoFileName) => {
               timestamps: [newTimestamp],
               filename: `frame-${newTimestamp}.png`,
               folder: frameImgPath,
-              size: `1280x720`
+              size: sizeByDimension
           });
       });  // return promise
       framesPromises.push(promise);
     }
-  };
+
+  }; //for
 
   return framesPromises;
 }
 
 // Crop all the faces from different frames
 // and save the images into a folder named by video filename in 'Faces' folder
-const cropFacesFromScreenshots = (faces, videoFileName) => {
+const cropFacesFromScreenshots = (faces, videoFileName, vidDimension) => {
 
     let oldTimestamp = -1;
     let sequence = 0;   // No. of face detected in the frame starts from 1
     console.log(`how many faces will be cropped: ${faces.length}`);
 
-    const faceImgPath = path.join(__dirname, 'Faces', videoFileName);
-    const frameImgPath = path.join(__dirname, 'Frames', videoFileName);
-    shell.mkdir(faceImgPath);
+    const faceImgPath = path.join(__demoRootDir, 'Faces', videoFileName);
+    const frameImgPath = path.join(__demoRootDir, 'Frames', videoFileName);
+    shell.mkdir('-p', faceImgPath);
 
     return faces.map((frame) => {
 
-      let size = helper.getFaceBoundary(frame.Face.BoundingBox);
+      let size = helper.getFaceBoundary(frame.Face.BoundingBox, vidDimension);
       frame.Timestamp = helper.msTohhmmssmmm(frame.Timestamp); 
 
       let newTimestamp = frame.Timestamp;
@@ -95,12 +129,13 @@ async function cropFacesFromLocalVideo (allFrames, videoFileName) {
 
   prepareDataDirectories();
 
-  let frames = await Promise.all(takeScreenshots(allFrames, videoFileName));
+  dimension = await getVideoDimension(videoFileName);
+  let frames = await Promise.all(takeScreenshots(allFrames, videoFileName, dimension));
   console.log(`Extracted ${frames.length} frames from video ${videoFileName}`);
 
-  let faces = await Promise.all(cropFacesFromScreenshots(allFrames, videoFileName));  
+  let faces = await Promise.all(cropFacesFromScreenshots(allFrames, videoFileName, dimension));  
   console.log(`Cropped ${faces.length} faces from video ${videoFileName}, Done!`);
-  const faceImgPath = path.join(__dirname, 'Faces', videoFileName);
+  const faceImgPath = path.join(__demoRootDir, 'Faces', videoFileName);
   let faces_bucket = await s3Client.createFolderInBucket(videoFileName, APP_FACES_BUCKET_NAME);
   
   let data = await s3Client.uploadMultiFiles(faceImgPath, APP_FACES_BUCKET_NAME, videoFileName);
@@ -113,6 +148,5 @@ async function cropFacesFromLocalVideo (allFrames, videoFileName) {
 // console.time('New image crop');
 // cropFacesFromLocalVideo(faces, 'sample-1.mp4');
 // console.log('done');
-
 
 module.exports = { cropFacesFromLocalVideo };
