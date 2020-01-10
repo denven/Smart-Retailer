da5dc5af1a28ba382eff5332ccb7f1b9c814796b
Chengwen <myregs@foxmail.com>
2020-01-10 12:26:39 -0800

    Added db operation for videos table

--- express-back-end/database/db.js
+++ express-back-end/database/db.js
@@ -2,16 +2,10 @@ const config = require('./knexfile');
 const env = process.env.NODE_ENV || 'development';
 const knex = require('knex')(config[env]);
 
-module.exports = function() {
-
-  //TODO: IMPLEMENT ALL THE QUERIES AND PROVIDE THEM AS FUNCTIONS FOR ROUTES METHODS  
-  // this.getAgeRangeFromVide = () => {}
-
-
-}
+const { getVideoDuration, getVideoFilmedDate, getVideoS3URL } = require('../rekognition/db-data');
 
 const testDBConnection = () => {
-
+  
   knex.raw("SELECT current_database()")
   .then( (data) => {
     let dbName = data.rows ?  data.rows[0].current_database : `Not Connected`;    
@@ -26,4 +20,47 @@ const testDBConnection = () => {
   .catch((err) => { console.log(err); throw err }); 
 }
 
-module.exports = { knex, testDBConnection};
+async function addOneVideoFile (videoName)  {
+  const duration = await getVideoDuration(videoName);
+  const filmedTime = getVideoFilmedDate(videoName); 
+  const videoS3Url = getVideoS3URL(videoName);
+
+  const videoRcord = {
+    name: videoName, 
+    duration: duration, 
+    filmed_at: filmedTime,
+    s3_url: videoS3Url,
+    ana_status: 0  //0: not analyzed; 1-2-3: in-process; 4: done, -1: failed
+  };
+  
+  console.log('Write videos table', videoRcord)
+  knex('videos').insert(videoRcord, ['id']).then(data => console.log(data)).catch(err => console.log(err));
+}
+
+/**
+ * If a task(having 4 jobs) has failed due to some job(s), it won't be updated even
+ * other jobs succeded.
+ * 
+ * @param {String} videoName 
+ * @param {Integer} status: 1: a job succeded, -1: a job failed
+ */
+const updateVideoAnaStatus = (videoName, status) => {
+
+  console.log(`update videos table, ${videoName}, ${status}`);
+
+  if(status === 1) {
+    knex('videos').select('ana_status').where('name', videoName)
+    .then( s => {
+      if(s !== -1) {
+        knex('videos').where('name', videoName).increment('ana_status', 1)
+        .then(s => console.log(`ana_statis, ${s}`))
+        .catch(err => console.log(err));
+      }
+    });
+  } else {
+    knex('videos').where('name', videoName).update('ana_status', status);
+  }
+
+}
+
+module.exports = { knex, testDBConnection, addOneVideoFile, updateVideoAnaStatus };
